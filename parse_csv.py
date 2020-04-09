c2ce8e7122973a0d9558e0838431c57936bbc077
Nick Young <nick.young@auckland.ac.nz>
2020-04-09 18:11:12 +1200

    2018 census update

--- parse_csv.py
+++ parse_csv.py
@@ -1,29 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import csv
 import sys
 import re
 import json
 
-fn = sys.argv[1]
+files = sys.argv[1:]
 o = {}
 
-with open(fn) as f:
-  reader = csv.DictReader(f)
-  for row in reader:
-    code = row['Code']
-    if len(code) == 6:
-      o[code] = {"desc": row['Description']}
-      for k,v in row.items():
-        if "religious" in k:
-          year = int(k[:4])
-          if year not in o[code]:
-            o[code][year] = {}
-          religion = k[107:]
-          if v == '..C':
-            v = None
-          else:
-            v = int(v)
-          o[code][year][religion] = v
+for fn in files:
+  with open(fn) as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+      #"Year","Area_type","Area_code","Area_description","Religious_affiliation_total_responses_code","Religious_affiliation_total_responses_description","Census_usually_resident_population_count","Religious_affiliation_total_responses_percent"
+      if row["Area_type"] == "Statistical Area 2":
+        code = row["Area_code"]
+        year = row["Year"]
+        religion = row["Religious_affiliation_total_responses_description"]
+        count = row["Census_usually_resident_population_count"]
+        if count == "C":
+          count = None
+        else:
+          count = int(count)
+        if code not in o:
+          o[code] = {}
+        if year not in o[code]:
+          o[code][year] = {}
+        o[code][year][religion] = count
 
 print(json.dumps(o, sort_keys=True, indent=4))
