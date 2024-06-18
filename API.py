4d08a506b8d51a66a8457534d70e89f6c3439bca
Nick Young <nick.young@auckland.ac.nz>
2024-06-18 17:09:55 +1200

    parquet for faster API startup

--- API.py
+++ API.py
@@ -7,48 +7,12 @@ import shapely
 from tqdm.auto import tqdm
 import time
 from osm2geojson import json2shapes
-import pandas as pd
-
-def load(prefix="churches"):
-    nodes = json.load(open(f"{prefix}_nodes.json"))
-    print(f'Loaded {len(nodes["elements"])} nodes')
-    ways = json.load(open(f"{prefix}_ways.json"))
-    print(f'Loaded {len(ways["elements"])} ways')
-    relations = json.load(open(f"{prefix}_relations.json"))
-    print(f'Loaded {len(relations["elements"])} relations')
-    shapes = []
-    try:
-        shapes.extend(json2shapes(nodes))
-    except:
-        print("Unable to parse nodes")
-    try:
-        shapes.extend(json2shapes(ways))
-    except:
-        print("Unable to parse ways")
-    try:
-        shapes.extend(json2shapes(relations))
-    except:
-        print("Unable to parse relations")
-    filtered_shapes = []
-    for feature in tqdm(shapes):
-        tags = feature["properties"]["tags"]
-        if feature["shape"].is_empty:
-            continue
-        filtered_shapes.append({
-            "lat": feature["shape"].centroid.y,
-            "lng": feature["shape"].centroid.x,
-            "name": tags.get("name"),
-            "religion": tags.get("religion"),
-            "denomination": tags.get("denomination"),
-            "start_date": tags.get("start_date")
-        })
-    df = pd.DataFrame(filtered_shapes)
-    return df
+import geopandas as gpd
 
 s = time.time()
-churches = load("churches")
-schools = load("schools")
-townhalls = load("townhalls")
+churches = gpd.read_parquet("churches.parquet")
+schools = gpd.read_parquet("schools.parquet")
+townhalls = gpd.read_parquet("townhalls.parquet")
 print(f"Loaded data in {time.time() - s} seconds")
 
 app = FastAPI(root_path="/OSM_API")
@@ -77,10 +41,15 @@ def get(bounds:str = "-90,-180,90,180", dataset:str = "churches", limit:int = 10
         data = townhalls
     else:
         raise HTTPException(status_code=400, detail="Invalid dataset")
+    s = time.time()
     filtered_data = data[
         data["lng"].between(bounds[0], bounds[2]) &
         data["lat"].between(bounds[1], bounds[3])
     ]
+    print(f"Filtered data in {time.time() - s} seconds")
     if len(filtered_data) > limit:
         filtered_data = filtered_data.sample(limit)
-    return filtered_data.to_dict(orient="records")
+        print(f"Sampled data in {time.time() - s} seconds")
+    records = filtered_data.drop(columns="geometry").to_dict(orient="records")
+    print(f"Converted data in {time.time() - s} seconds")
+    return records
